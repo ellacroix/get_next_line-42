1c399674cb13426d9f5193b21b9341b4772b360e
Eliot <eliot.lacroix@essec.edu>
2020-11-22 19:14:26 +0100

    Stable

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,94 @@
 /*   By: ellacroi <ellacroi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 18:51:16 by ellacroi          #+#    #+#             */
-/*   Updated: 2020/11/19 18:51:17 by ellacroi         ###   ########.fr       */
+/*   Updated: 2020/11/22 19:12:40 by ellacroi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#define BUFFER_SIZE 2
+
+int	ft_remove_line_from_buffer(char *buffer)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer[i] != '\n' && i <BUFFER_SIZE)
+		i++;
+	if (buffer[i] == '\0')
+		return (0);
+	while (++i < BUFFER_SIZE)
+		buffer[j++] = buffer[i];
+	while (j < BUFFER_SIZE)
+		buffer[j++] = '\0';
+	return (1);
+}
+
+int	ft_buffer_to_line(char *buffer, char **line)
+{
+	char	*newline;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (buffer[i] && buffer[i] != '\n' && i <BUFFER_SIZE)
+		i++;
+	while (*line && (*line)[j])
+		j++;
+	if (!(newline = malloc(sizeof(char) * (i + j + 1))))
+		return (-1);
+	newline[i + j] = '\0';
+	while (i--)
+		newline[j + i] = buffer[i];
+	while (j--)
+		newline[j] = (*line)[j];
+	if (*line)
+		free(*line);
+	*line = newline;
+	return(ft_remove_line_from_buffer(buffer));
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	buffer[BUFFER_SIZE];
+	int			ret;
+
+	if (!line || fd < 0 || BUFFER_SIZE < 1)
+		return (-1);
+	*line = NULL;
+	ret = 0;
+	while ((ret = ft_buffer_to_line(buffer, line)) == 0)
+	{
+		if ((ret = read(fd, buffer, BUFFER_SIZE)) < 1)
+		{
+			if (ret == -1)
+			{
+				if (*line)
+					free(*line);
+				line = NULL;
+			}
+			return (ret);
+		}
+	}
+	return (ret);
+}
+
+int	main(int ac, char **av)
+{
+	(void)ac;
+	int	fd = open(av[1], O_RDONLY);
+	int	fd2 = open(av[2], O_RDONLY);
+	char *line = NULL;
+
+	get_next_line(fd, &line);
+	get_next_line(fd, &line);
+	get_next_line(fd2, &line);
+	printf("line = %s\n", line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ellacroi <ellacroi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/19 18:54:11 by ellacroi          #+#    #+#             */
+/*   Updated: 2020/11/22 18:47:40 by ellacroi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFFER_SIZE 2
+
+#endif
